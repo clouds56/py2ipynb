461b7ca0bfd90850297fb6aeee99ec89ba0b1c01
Clouds <clouds.flowing@gmail.com>
2016-11-14 20:33:49 +0800

    impore file structrue

--- pavement.py
+++ pavement.py
@@ -1,20 +1,35 @@
 from paver.easy import *
 
+src = path("src")
+bin = path("bin")
 basename = "py2ipynb"
+exehead = "#!/usr/bin/python"
+
+def makesure_bin():
+    if not bin.exists():
+        bin.mkdir()
 
 def compile(input, output):
+    makesure_bin()
     if not path(output).exists() or path(input).mtime > path(output).mtime:
-        sh(["./py2ipynb", input, "-o", output, "--verb"])
+        sh([bin / basename, input, "-o", output, "--verb"])
     else:
         info('%s is latest' % output)
 
 @task
 def build():
-    compile("%s.ipynb" % basename, "%s.py" % basename)
+    compile(src / "%s.ipynb" % basename, bin / "%s.py" % basename)
+
+class Lines(list):
+    def __repr__(self):
+        return "<%d lines>" % len(self)
 
 @task
 @needs("build")
 def install():
-    sh("echo '#!/usr/bin/python' > '%s'" % basename)
-    sh("cat '{0}.py' >> '{0}'".format(basename))
-    sh("chmod +x '%s'" % basename)
+    makesure_bin()
+    exefile = bin / basename
+    lines = Lines((bin / ("%s.py" % basename)).lines())
+    lines[0] = exehead
+    exefile.write_lines(lines, linesep='\n')
+    sh("chmod +x '%s'" % exefile)
