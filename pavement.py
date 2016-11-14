91fe658374ea795d36874d2adf7e90223d552179
Clouds <clouds.flowing@gmail.com>
2016-11-14 19:58:36 +0800

    paver check file modified time

--- pavement.py
+++ pavement.py
@@ -2,11 +2,18 @@ from paver.easy import *
 
 basename = "py2ipynb"
 
-@task
-def build():
-    sh(["./py2ipynb", "%s.ipynb" % basename, "-o", "%s.py" % basename, "--verb"])
+def compile(input, output):
+    if not path(output).exists() or path(input).mtime > path(output).mtime:
+        sh(["./py2ipynb", input, "-o", output, "--verb"])
+    else:
+        info('%s is latest' % output)
 
 @task
+def build():
+    compile("%s.ipynb" % basename, "%s.py" % basename)
+
+@task
+@needs("build")
 def install():
     sh("echo '#!/usr/bin/python' > '%s'" % basename)
     sh("cat '{0}.py' >> '{0}'".format(basename))
